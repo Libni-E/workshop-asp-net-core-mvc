ec55a39f17c1005fab0d987a94a060c3270eab52
Libni.Dev <libnievanio@gmail.com>
2024-04-06 08:22:48 -0300

    Criando app

---
+++ Aplicacao-Web-AspNet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Aplicacao_Web_AspNet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
