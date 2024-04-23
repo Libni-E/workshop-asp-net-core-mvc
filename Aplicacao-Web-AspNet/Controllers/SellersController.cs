727bd94923baae597bbe03601b929452e742c9b0
Libni.Dev <libnievanio@gmail.com>
2024-04-23 15:07:04 -0300

    Sellercontroller

---
+++ Aplicacao-Web-AspNet/Controllers/SellersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aplicacao_Web_AspNet.Controllers
+{
+    public class SellersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
