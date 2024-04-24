efdf90cf4ad23d7e92ac696617e9825df76c57bb
Libni.Dev <libnievanio@gmail.com>
2024-04-24 19:55:32 -0300

    Preparing sales search navigation views

---
+++ Aplicacao-Web-AspNet/Controllers/SalesRecordsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aplicacao_Web_AspNet.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SimpleSearch()
+        {
+            return View();
+        }
+
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
